efafde66ccea6ddc095dd4dfec6f250a21cecae6
infomediabl <bojan.milekic@gmail.com>
2025-04-26 19:41:25 +0200

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/quizv2/', // Replace with your repository name
+  base: '/thitny-v3/',
 })
